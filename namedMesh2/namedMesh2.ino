69b311642cd7bc435fe7fa019136fa2c085fd720
Your Name <you@example.com>
2019-01-10 16:03:42 +0100

    Basic Mesh Configuration

---
+++ namedMesh2/namedMesh2.ino
@@ -0,0 +1,54 @@
+//************************************************************
+// this is a simple example that uses the painlessMesh library
+// 
+// This example shows how to build a mesh with named nodes
+//
+//************************************************************
+#include "namedMesh.h"
+
+#define   MESH_SSID       "whateverYouLike"
+#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PORT       5555
+
+Scheduler     userScheduler; // to control your personal task
+namedMesh  mesh;
+
+String nodeName = "2"; // Name needs to be unique
+
+Task taskSendMessage( TASK_SECOND*5, TASK_FOREVER, []() {
+    Serial.println("Task pop up\n");
+    String msg = String("This is a message from: ") + nodeName + String(" for logNode 1");
+    String to = "1";
+    //mesh.sendSingle(to, msg); 
+     mesh.sendBroadcast(msg); 
+}); // start with a one second interval
+
+void setup() {
+  Serial.begin(115200);
+
+  //mesh.setDebugMsgTypes(ERROR | DEBUG | CONNECTION);  // set before init() so that you can see startup messages
+
+  mesh.init(MESH_SSID, MESH_PASSWORD, &userScheduler, MESH_PORT);
+
+  mesh.setName(nodeName); // This needs to be an unique name! 
+
+  mesh.onReceive([](uint32_t from, String &msg) {
+    Serial.printf("Received message by id from: %u, %s\n", from, msg.c_str());
+  });
+
+  mesh.onReceive([](String &from, String &msg) {
+    Serial.printf("Received message by name from: %s, %s\n", from.c_str(), msg.c_str());
+  });
+
+  mesh.onChangedConnections([]() {
+    Serial.printf("Changed connection\n");
+  });
+
+  userScheduler.addTask(taskSendMessage);
+  taskSendMessage.enable();
+}
+
+void loop() {
+  userScheduler.execute(); // it will run mesh scheduler as well
+  mesh.update();
+}
